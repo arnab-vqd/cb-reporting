a7f38705743f538b99575ac82e9546f74dee2f36
Arnab Chakraborty <arnab.chakraborty@viqtordavis.com>
2022-03-19 16:54:03 +0000

    Integrated API's

--- src/app/chartcontainer/chartcontainer.component.ts
+++ src/app/chartcontainer/chartcontainer.component.ts
@@ -1,28 +1,39 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chartcontainer.component.html',
   styleUrls: ['./chartcontainer.component.scss']
 })
-export class ChartcontainerComponent implements OnInit {
-  @Input('chart_label') chartLabel: any = '';
+export class ChartcontainerComponent implements OnInit, OnChanges {
+
+  @Input()
+  chartLabel: any = '';
+
   type = 'bar';
-  data = {
-    labels: ['Total Sale', 'Food Sale', 'Beverage Sale', 'Hookah Sale', 'Buffet Sale', 'Liquor Sales'],
-    datasets: [
-      {
-        label: 'Dine in',
-        data: [48741, 18312, 11977, 15519, 108, 2825],
-        backgroundColor: 'rgb(202 21 63)'
-      },
-      {
-        label: 'Delivery',
-        data: [12781, 9403, 1568, 15, 1655, 0],
-        backgroundColor: 'rgb(6 126 246)'
-      }
-    ]
-  };
+
+  @Input()
+  chartData ;
+
+  data;
+  updateChart(){
+    this.data = {
+      labels: ['Total Sale', 'Food Sale', 'Beverage Sale', 'Hookah Sale', 'Buffet Sale', 'Liquor Sales'],
+      datasets: [
+        {
+          label: 'Dine in',
+          data: this.chartData.dineIn,
+          backgroundColor: 'rgb(202 21 63)'
+        },
+        {
+          label: 'Delivery',
+          data: this.chartData.delivery,
+          backgroundColor: 'rgb(6 126 246)'
+        }
+      ]
+    };
+  }
+
   options = {
     responsive: true,
     maintainAspectRatio: false,
@@ -41,6 +52,13 @@ export class ChartcontainerComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.updateChart();
   }
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.chartData = changes.chartData.currentValue;
+    this.updateChart();
+  }
+
 
 }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -20,40 +20,11 @@ export class DashboardComponent implements OnInit {
               private contentControllerService: ContentControllerService,
               private salesReportControllerService: SalesReportControllerService) { }
 
-  cardList: any = [{
-    title: 'Food Sale',
-    categoryIcon: 'fa fa-glass',
-    categoryTitle: 'Food',
-    count: '10239',
-    unitType: 'Quantity'
-  }, {
-    title: 'Beverage Sale',
-    categoryIcon: 'fa fa-glass',
-    categoryTitle: 'Liquor',
-    count: '23434',
-    unitType: 'Quantity'
-  }, {
-    title: 'Hookah Sale',
-    categoryIcon: 'fa fa-glass',
-    categoryTitle: 'Liquor',
-    count: '3434',
-    unitType: 'Quantity'
-  }, {
-    title: 'Buffet Sale',
-    categoryIcon: 'fa fa-glass',
-    categoryTitle: 'Liquor',
-    count: '4234',
-    unitType: 'Quantity'
-  }, {
-    title: 'Liquor Sale',
-    categoryIcon: 'fa fa-glass',
-    categoryTitle: 'Liquor',
-    count: '13123',
-    unitType: 'Quantity'
-  }];
+  cardList: any = [];
+  chartData = {dineIn: [0, 0, 0, 0, 0], delivery: [0, 0, 0, 0, 0]};
 
   ngOnInit() {
-
+    this.reCalculateCards();
     this.contentControllerService.getAllLocationsUsingGET().subscribe(obj => {
       this.outletList = obj;
     });
@@ -79,8 +50,81 @@ export class DashboardComponent implements OnInit {
     });
     const data: SalesReportRequestParams =  {compareLastYear: false, outlet: outletList.join(','),
        startDate: reportStartDate, toDate: reportToDate, valueType: value };
-    this.salesReportControllerService.getReportBeverageUsingPOST( data).subscribe(response => {
-      console.log(response);
+    this.salesReportControllerService.getReportFoodUsingPOST( data).subscribe(response => {
+      this.foodDelivery = response['Food Sale HD'].key;
+      this.foodDineIn = response['Food Sale DI'].key;
+      this.reCalculateCards();
     });
+    this.salesReportControllerService.getReportBeverageUsingPOST( data).subscribe(response => {
+      this.beverageDelivery = response['Beverage Sale HD'].key;
+      this.beverageDineIn = response['Beverage Sale DI'].key;
+      this.reCalculateCards();
+    });
+    this.salesReportControllerService.getReportHookahUsingPOST( data).subscribe(response => {
+      this.hookahDelivery = response['Hookah Sale HD'].key;
+      this.hookahDineIn = response['Hookah Sale DI'].key;
+      this.reCalculateCards();
+    });
+    this.salesReportControllerService.getReportLiquorUsingPOST( data).subscribe(response => {
+      this.liquorDelivery = response['Liquor Sale HD'].key;
+      this.liquorDineIn = response['Liquor Sale DI'].key;
+      this.reCalculateCards();
+    });
+
+    this.salesReportControllerService.getReportBuffetUsingPOST( data).subscribe(response => {
+      this.buffetDelivery = response['Buffet Sale HD'].key;
+      this.buffetDineIn = response['Buffet Sale DI'].key;
+      this.reCalculateCards();
+    });
+  }
+
+  foodDineIn: any = 0;
+  foodDelivery: any = 0;
+  beverageDineIn: any = 0;
+  beverageDelivery: any = 0;
+  hookahDineIn: any = 0;
+  hookahDelivery: any = 0;
+  buffetDineIn: any = 0;
+  buffetDelivery: any = 0;
+  liquorDineIn: any = 0;
+  liquorDelivery: any = 0;
+
+  reCalculateCards() {
+    this.chartData = {
+      dineIn: [this.foodDineIn, this.beverageDineIn, this.hookahDineIn, this.buffetDineIn, this.liquorDineIn],
+      delivery: [this.foodDelivery, this.beverageDelivery, this.hookahDelivery, this.buffetDelivery, this.liquorDelivery]
+    };
+
+    this.cardList = [{
+      title: 'Food Sale',
+      categoryIcon: 'fa fa-glass',
+      categoryTitle: 'Delivery ('+this.foodDelivery+')',
+      count: this.foodDineIn,
+      unitType: 'Quantity'
+    }, {
+      title: 'Beverage Sale',
+      categoryIcon: 'fa fa-glass',
+      categoryTitle: 'Delivery ('+this.beverageDelivery+')',
+      count: this.beverageDineIn,
+      unitType: 'Quantity'
+    }, {
+      title: 'Hookah Sale',
+      categoryIcon: 'fa fa-glass',
+      categoryTitle: 'Delivery ('+this.hookahDelivery+')',
+      count: this.hookahDineIn,
+      unitType: 'Quantity'
+    }, {
+      title: 'Buffet Sale',
+      categoryIcon: 'fa fa-glass',
+      categoryTitle: 'Delivery ('+this.buffetDelivery+')',
+      count: this.buffetDineIn,
+      unitType: 'Quantity'
+    }, {
+      title: 'Liquor Sale',
+      categoryIcon: 'fa fa-glass',
+      categoryTitle: 'Delivery ('+this.liquorDelivery+')',
+      count: this.liquorDineIn,
+      unitType: 'Quantity'
+    }];
   }
 }
